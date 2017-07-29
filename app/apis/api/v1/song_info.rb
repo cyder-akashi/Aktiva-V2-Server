b337b088be5d5906936de477c90161c9ba6e6e50
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-07-30 01:22:43 +0900

    [add] 作詞、作曲、振付師表示

--- app/apis/api/v1/song_info.rb
+++ app/apis/api/v1/song_info.rb
@@ -8,6 +8,18 @@ module API
         end
         get '/', jbuilder: 'api/v1/songInfo' do
           @song = Song.find(params[:songId])
+          if not @song.artist_id.nil? then
+            @artist = Person.find(@song.artist_id)
+          end
+          if not @song.lyricist_id.nil? then
+            @lyricist = Person.find(@song.lyricist_id)
+          end
+          if not @song.composer_id.nil? then
+            @composer = Person.find(@song.composer_id)
+          end
+          if not @song.choreographer_id.nil? then
+            @choreographer = Person.find(@song.choreographer_id)
+          end
         end
       end
     end
--- app/views/apis/api/v1/songInfo.jbuilder
+++ app/views/apis/api/v1/songInfo.jbuilder
@@ -1,1 +1,22 @@
-json.(@song, :song_id, :title)
+json.set! :song_id, @song.song_id
+json.set! :title, @song.title
+if not @artist.nil? then
+  json.artist(@artist, :id, :name)
+else
+  json.set! :artist, nil
+end
+if not @lyricist.nil? then
+  json.lyricist(@lyricist, :id, :name)
+else
+  json.set! :lyricist, nil
+end
+if not @composer.nil? then
+  json.composer(@composer, :id, :name)
+else
+  json.set! :composer, nil
+end
+if not @horeographer.nil? then
+  json.choreographer(@choreographer, :id, :name)
+else
+  json.set! :choreographer, nil
+end
