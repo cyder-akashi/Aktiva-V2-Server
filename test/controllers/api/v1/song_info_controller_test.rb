d7dae53f1bdfb4db33d082303ad390bb342ef0f6
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-08-03 00:13:47 +0900

    [add] テスト用DB作成と簡易テスト実施

--- test/controllers/api/v1/song_info_controller_test.rb
+++ test/controllers/api/v1/song_info_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class Api::V1::SongInfoControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @song = songs(:one)
+  end
+
   test "should get show" do
-    get song_info_show_url
+    get api_v1_song_info_show_url, params: { song_id: @song.song_id }
     assert_response :success
   end
 
