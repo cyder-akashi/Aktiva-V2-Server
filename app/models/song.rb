c5646578f5bdb376c786d15b43411aaf75a0a676
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-08-04 20:05:53 +0900

    [fix] バリデーション修正

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,8 @@
 class Song < ApplicationRecord
   belongs_to :artist, class_name: "Person", foreign_key: 'artist_id'
-  belongs_to :choreographer, class_name: "Person", foreign_key: 'choreographer_id'
-  belongs_to :composer, class_name: "Person", foreign_key: 'composer_id'
-  belongs_to :lyricist, class_name: "Person", foreign_key: 'lyricist_id'
+  belongs_to :choreographer, class_name: "Person", foreign_key: 'choreographer_id', optional: true
+  belongs_to :composer, class_name: "Person", foreign_key: 'composer_id', optional: true
+  belongs_to :lyricist, class_name: "Person", foreign_key: 'lyricist_id', optional: true
   has_many :lyrics
   validates :title, presence: true
-  validates :beginners_badge, presence: true
-  validates :person_badge, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
 num = 5
 
 num.times do |no|
-  Person.create name: "名前 #{no}"
-end
-
-num.times do |no|
-  Song.create title: "タイトル #{no}", artist_id: no + 1
+  person = Person.create name: "名前 #{no}"
+  Song.create title: "タイトル #{no}", artist: person
 end
