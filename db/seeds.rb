faba3609d484027954c9b1992815ef68cd158631
crome110 <crome110mine@gmail.com>
2017-08-05 20:22:22 +0900

    [add] 歌詞初期データの挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
-num = 5
+song_num = 5
+lyrics_num = 5
 
-num.times do |no|
+
+song_num.times do |no|
   person = Person.create name: "名前 #{no}"
-  Song.create title: "タイトル #{no}", artist: person
+  song = Song.create title: "タイトル #{no}", artist: person
+  lyrics_num.times do |lyrics_no|
+    Lyric.create song: song, time: "00:#{lyrics_no}", text: "test_lyrics #{lyrics_no}"
+  end
 end
